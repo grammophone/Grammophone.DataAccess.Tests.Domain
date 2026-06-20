86fd8f6ea0d316be6ceb48ac362974c28c38ebd1
grammophone <emmanuel.liossis@gmail.com>
2026-06-20 13:25:57 -0400

    Unit tests for batch operations.

--- TestData.cs
+++ TestData.cs
@@ -22,6 +22,27 @@ namespace Grammophone.DataAccess.Tests.Domain
 		/// <summary>Track duration in seconds.</summary>
 		public const int TrackDurationSeconds = 245;
 
+		/// <summary>Updated track duration in seconds.</summary>
+		public const int UpdatedTrackDurationSeconds = 300;
+
+		/// <summary>Batch operation artist name prefix.</summary>
+		public const string SetOperationArtistNamePrefix = "Set Operation Artist";
+
+		/// <summary>Batch operation album name prefix.</summary>
+		public const string SetOperationAlbumNamePrefix = "Set Operation Album";
+
+		/// <summary>Batch operation original genre name prefix.</summary>
+		public const string SetOperationOriginalGenreNamePrefix = "Set Operation Original Genre";
+
+		/// <summary>Batch operation target genre name prefix.</summary>
+		public const string SetOperationTargetGenreNamePrefix = "Set Operation Target Genre";
+
+		/// <summary>First batch operation track name prefix.</summary>
+		public const string SetOperationTrackOneNamePrefix = "Set Operation Track One";
+
+		/// <summary>Second batch operation track name prefix.</summary>
+		public const string SetOperationTrackTwoNamePrefix = "Set Operation Track Two";
+
 		/// <summary>Album release date.</summary>
 		public static readonly DateTime AlbumReleaseDate = new DateTime(2024, 1, 1);
 	}
